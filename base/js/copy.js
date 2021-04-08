34c0e7042336d054da97713be66a7bc89521c35a
wangtao <284524637@qq.com>
2021-04-09 00:11:24 +0800

    feat: copy

--- base/js/copy.js
+++ base/js/copy.js
@@ -30,7 +30,7 @@ let d = JSON.parse(JSON.stringify(deep))
 
 deep.jobs.first = 'native'
 
-console.log('d',d.jobs.first);
+console.log('d', d.jobs.first);
 
 // JSON.parse(JSON.stringify(deep) 方法局限性
 // 忽略 undifined
@@ -41,76 +41,76 @@ console.log('d',d.jobs.first);
 let obj = {
     a: 1,
     b: {
-      c: 2,
-      d: 3,
+        c: 2,
+        d: 3,
     },
-  }
-  obj.c = obj.b
-  obj.e = obj.a
-  obj.b.c = obj.c
-  obj.b.d = obj.b
-  obj.b.e = obj.b.c
-  let newObj = JSON.parse(JSON.stringify(obj)) // 报错
-  console.log(newObj) 
+}
+obj.c = obj.b
+obj.e = obj.a
+obj.b.c = obj.c
+obj.b.d = obj.b
+obj.b.e = obj.b.c
+let newObj = JSON.parse(JSON.stringify(obj)) // 报错
+console.log(newObj)
 
-  let e = {
+let e = {
     age: undefined,
     sex: Symbol('male'),
-    jobs: function() {},
+    jobs: function () { },
     name: 'yck'
-  }
-  let f = JSON.parse(JSON.stringify(e))
-  console.log(f) // {name: "yck"}
+}
+let f = JSON.parse(JSON.stringify(e))
+console.log(f) // {name: "yck"}
 
-  // MessageChannel
-function structturalClone(obj){
-    return new Promise(resolve =>{
-        const {port1 = null, port2 = null} = new MessageChannel()
+// MessageChannel
+function structturalClone(obj) {
+    return new Promise(resolve => {
+        const { port1 = null, port2 = null } = new MessageChannel()
         port2.onmessage = ev => resolve(ev.data)
         port1.postMessage(obj)
     })
 }
-  var objM = {
+var objM = {
     a: 1,
     b: {
-      c: 2
+        c: 2
     }
-  }
-  
-  objM.b.d = objM.b
+}
 
-  const test = async ()=>{
-      const clone = await structturalClone(objM)
-      console.log('clone',clone);
-  }
-  
-  test()
+objM.b.d = objM.b
 
-  // 实现简单的deepClone
-  function deepClone(obj){
-      function isObject(obj){
-          return (typeof obj === 'object' || typeof obj === 'function') && obj !== null
-      }
+const test = async () => {
+    const clone = await structturalClone(objM)
+    console.log('clone', clone);
+}
 
-      if(!isObject(obj)){
+test()
+
+// 实现简单的deepClone
+function deepClone(obj) {
+    function isObject(obj) {
+        return (typeof obj === 'object' || typeof obj === 'function') && obj !== null
+    }
+
+    if (!isObject(obj)) {
         throw new Error('非对象');
-      }
+    }
 
-      const isArray = Array.isArray(obj)
-      let newObj = isArray?[...obj]:{...obj}
-      Reflect.ownKeys(newObj).forEach(item =>{
-          newObj[item] = isObject(newObj[item])?deepClone(newObj[item]): newObj[item]
-      })
+    const isArray = Array.isArray(obj)
+    let newObj = isArray ? [...obj] : { ...obj }
+    Reflect.ownKeys(newObj).forEach(item => {
+        newObj[item] = isObject(newObj[item]) ? deepClone(newObj[item]) : newObj[item]
+    })
 
-      return newObj
-  }
-  let objD = {
+    return newObj
+}
+let objD = {
     a: [1, 2, 3],
     b: {
-      c: 2,
-      d: 3
+        c: 2,
+        d: 3
     }
-  }
-  let newObj = deepClone(objD)
-  objD.b.c = 1;
-  console.log(newObj.b.c);
+}
+let newObjD = deepClone(objD)
+objD.b.c = 1;
+console.log(newObjD.b.c);
