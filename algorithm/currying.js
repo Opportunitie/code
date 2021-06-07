03055098ad65be20410df9be87530deeb38551b4
wangtao <tao.wang@vbooster.cn>
2021-06-07 10:28:04 +0800

    feat: currying

--- algorithm/currying.js
+++ algorithm/currying.js
@@ -5,3 +5,17 @@
 // add(1)(2)(3)(4)(5) = 15;
 
 
+function currying(){
+  let data = [... Array.from(arguments)]
+  let _add = function (){
+    if(arguments.length === 0){
+      return data.reduce((sum, current) => sum + current, 0)
+    }else{
+      data = data.concat(Array.from(arguments))
+      return _add
+    }
+  }
+  return _add
+}
+
+console.log(currying(1)(2)(3)());
