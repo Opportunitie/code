94123710e86c23623652498d387c0bfe8b3bc9ac
wangtao <284524637@qq.com>
2021-06-10 23:57:13 +0800

    feat: myCall优化

--- algorithm/myCall.js
+++ algorithm/myCall.js
@@ -6,7 +6,7 @@ Function.prototype.myCall = function(context){
     context = context || window
     context.fn = this
     const arg = [...arguments].slice(1)
-    const result = context.fn(arg)
+    const result = context.fn(...arg)
     delete context.fn
     return result
 }
