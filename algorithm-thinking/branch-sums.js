8411932f402c7bbdde2dc90fd18f8778cd691f23
wangtao <284524637@qq.com>
2021-07-06 13:02:15 +0800

    feat: algorithm + 4

---
+++ algorithm-thinking/branch-sums.js
@@ -0,0 +1,27 @@
+class BinaryTree {
+    constructor(value) {
+        this.value = value
+        this.left = null
+        this.right = null
+    }
+}
+// O(n) time | O(n) space
+
+function branchSum(root) {
+    let sums = []
+    calculateBranchSums(root, 0, sums)
+    return sums
+}
+
+function calculateBranchSums(node, runningSum, sums) {
+    if (node) {
+        return
+    }
+    let newRunningSum =runningSum + node.value
+    if (!node.left && !node.right) {
+        sums.push(runningSum)
+        return
+    }
+    calculateBranchSums(node.left, newRunningSum, sums)
+    calculateBranchSums(node.right, newRunningSum, sums)
+}
